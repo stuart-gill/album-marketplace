06433c36cae767547164a6d76298a40a2e412546
Guest User <Guest@epicodus-79.local>
2019-01-24 15:49:12 -0800

    dependency injection, service, provider to move album information throughout application

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent }   from './about/about.component';
 import { MarketplaceComponent }   from './marketplace/marketplace.component';
